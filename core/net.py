155bb98f4a95f630b1d19dbdd6ae7acebf68b65f
hargathor <3949704+hargathor@users.noreply.github.com>
2018-01-27 11:37:28 +0100

    Implementing xacml send request using requests library

--- core/net.py
+++ core/net.py
@@ -21,9 +21,12 @@
 # Last Modified Date: 26.11.2017
 # Last Modified By  : hargathor <3949704+hargathor@users.noreply.github.com>
 
+import json
 import logging
-# import requests
+import requests
 import sys
+
+from pyxacml_sdk.core.utils import Tools
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -32,6 +35,35 @@ class Net(object):
 
     """Docstring for sdk. """
 
-    def __init__(self, endpoint, domain_id, port=8080, headers=None):
+    def __init__(self, host, endpoint, domain_id, port=8080, headers=None):
         """TODO: to be defined1. """
         logging.debug('Initializing Network client....')
+        self.tcp_ip = host
+        self.tcp_port = port
+        self.endpoint = endpoint
+        self.domain_id = domain_id
+        self.headers = {}
+        self.headers['Content-Type'] = 'Application/json'
+        self.headers['Accept'] = 'Application/json'
+        if headers:
+            self.headers.update(headers)
+        print(self.headers)
+        self.ssl_enable = ""
+
+    def send_request(self, xacml_request):
+        """TODO: Docstring for send_request.
+
+        :payload: TODO
+        :returns: TODO
+
+        """
+        payload = json.dumps(xacml_request, default=Tools.default_serializer)
+        r = requests.post(
+            'http://{host}:{port}/{endpoint}/domains/{domain}/pdp'.format(
+                host=self.tcp_ip, port=self.tcp_port, endpoint=self.endpoint, domain=self.domain_id),
+            headers=self.headers,
+            data=payload)
+        if r.status_code != 200:
+            logging.error("Something went wrong: {}".format(r.reason))
+        logging.debug(r.raw)
+        logging.debug("Response from server: {}".format(r.json()))
